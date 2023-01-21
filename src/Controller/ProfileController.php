67a14ff774cb2cbf5e1d7afc4977197c5d482e39
Merv_ <marvine.gaudree.pro@outlook.com>
2023-01-21 18:26:51 +0100

    [ADD] profil with img+edit+visio+classement

--- src/Controller/ProfileController.php
+++ src/Controller/ProfileController.php
@@ -2,21 +2,47 @@
 
 namespace App\Controller;
 
+use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 use App\Entity\User;
+use App\Form\EditProfileType;
+use Doctrine\ORM\EntityManagerInterface;
+use Doctrine\SqlFormatter\Token;
 
 class ProfileController extends AbstractController
 {
     #[Route('/profil', name: 'app_profile')]
-    public function index(): Response
+    public function index(ManagerRegistry $doctrine): Response
     {
         $user = $this->getUser();
+        $repo = $doctrine->getManager()->getRepository(User::class);
+        $listUsers = $repo->findAll();
+
         return $this->render('profile/index.html.twig', [
             'controller_name' => 'ProfileController',
-            'user' => $user
+            'user' => $user,
+            'listUsers' => $listUsers
+        ]);
+    }
+
+    #[Route('/edit-profile', name: 'app_profile_edit')]
+    public function edit_profile(ManagerRegistry $doctrine, Request $request, EntityManagerInterface $manager): Response
+    {    
+        $user = $this->getUser();
+        $form = $this->createForm(EditProfileType::class, $user);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $manager->persist($user);
+            $manager->flush();
+            return $this->redirectToRoute('app_profile');
+        }
+        return $this->render('profile/edit.html.twig', [
+            'form' => $form
         ]);
     }
 }
